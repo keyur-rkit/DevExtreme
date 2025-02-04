41f0c7bc5b825c881a8ebcc87d9f05c07b1399c2
keyursaradva <saradvakeyur@gmail.com>
2025-02-04 18:55:39 +0530

    Add project files.

---
+++ EditorsOverview/CheckBox/CheckBox.js
@@ -0,0 +1,73 @@
+﻿$(document).ready(function () {
+    $("#checkBox").dxCheckBox({
+        value: undefined, // true, false(same as "null"), undefined
+        disabled: false,
+        elementAttr: {
+            class: "dummy",
+            name: "CheckBox", // name added to #checkBox
+            // id: "dummy" // override original
+        }, // Specifies attributes
+        focusStateEnabled: true, // focused using keyboard navigation
+        // height: "50px", // string , number , function
+        // weight: "50px",
+        accessKey: 'k', // shortcut key (Alt + char)
+        hint: "Alt + k", // like tooltip
+        name: "checkBox", // name added to one hidden input
+        tabIndex: 2, // number of element when Tab key is used for navigating
+        text: "Age>18?",
+        visible: true,
+        rtlEnabled: true, // right to left
+    });
+
+    $("#checkBox2").dxCheckBox({
+        tabIndex: 1
+    });
+
+    $('#checkBox3').dxCheckBox();
+
+    // Gets the UI component's instance.
+    var checkBoxInstance = $('#checkBox').dxCheckBox("instance");
+    var checkBox2Instance = $('#checkBox2').dxCheckBox("instance");
+    var checkBox3Instance = $('#checkBox3').dxCheckBox("instance");
+
+    // Get html/jQuery element
+    var elementBox3 = checkBox3Instance.element();
+
+    // valueChanged event, on method to subscribes event
+    checkBox2Instance.on("valueChanged", function () {
+        checkBoxInstance.focus(); // get focus
+        checkBoxInstance.reset(); // reset value to default
+
+        // Begin the update
+        checkBox3Instance.beginUpdate();
+
+        // Make multiple changes
+        checkBox3Instance.option("value", true);
+        checkBox3Instance.option("text", "Updated Text");
+        checkBox3Instance.option("disabled", false);
+
+        // End the update and render changes
+        checkBox3Instance.endUpdate();
+    });
+
+    // to remove event
+    // checkBox2Instance.off("valueChanged");
+
+    // Gets value of a single property
+    console.log("Getting text using option, text: " + checkBoxInstance.option("text"));
+
+    // Registers a handler to be executed when a user presses a specific key.
+    checkBox3Instance.registerKeyHandler("del", function () {
+        checkBox3Instance.option("text", "changed text");
+    });
+
+    // Raised after a UI component property is changed.
+    checkBox3Instance.on("optionChanged", function () {
+        console.log("option changed");
+    });
+
+    // dispose() method
+    // $("#checkBox2").dxCheckBox("dispose");
+    // $("#CheckBox2").remove();
+
+});
---
+++ EditorsOverview/DateBox/DateBox.js
@@ -0,0 +1,96 @@
+﻿$(document).ready(function () {
+    const now = new Date();
+
+    $('#dateBox').dxDateBox({
+        type: 'date', // date time datetime
+        acceptCustomValue: false,
+        accessKey: 't',
+        applyValueMode: 'useButtons', // default: instantly
+        max: new Date(),
+        min: new Date(1900, 0, 1),
+        dateOutOfRangeMessage: 'date is out of range',
+        disabled: true, 
+        disabledDates: [
+            new Date('02/02/2025') // mm/dd/yyyy
+        ],
+        disabledDates: function (args) {
+            const dayOfWeek = args.date.getDay();
+            const isWeekend = args.view === 'month' && (dayOfWeek === 0 || dayOfWeek === 6);
+            return isWeekend;
+        },
+        elementAttr: {
+            name: 'dateBox'
+        },
+        height: '50px',
+        width: '500px',
+        hint: 'Date Box',
+        inputAttr: {
+            id: "inputId"
+        },
+        openOnFieldClick: true,
+        placeholder: '7/15/2004',
+        rtlEnabled: true,
+        showClearButton: true,
+        stylingMode: 'filled',  // outlined underlined filled
+        useMaskBehavior: true // can only type in format
+    });
+
+    $('#timeBox').dxDateBox({
+        type: 'time',
+        interval: 10, // default 30
+        pickerType: 'rollers', // native calendar rollers list
+        value: now,
+    });
+
+    $('#datetimeBox').dxDateBox({
+        type: 'datetime',
+        applyButtonText: '👍',
+        cancelButtonText: '❌',
+        displayFormat: 'EEEE, d of MMM, yyyy HH:mm',
+        invalidDateMessage: 'Date or time is invalid',
+        maxLength: 6,
+        opened: true,
+    });
+
+    // instance method
+    var dateBoxInstance = $("#dateBox").dxDateBox("instance");
+    var timeBoxInstance = $("#timeBox").dxDateBox("instance");
+    var datetimeBoxInstance = $("#datetimeBox").dxDateBox("instance");
+
+    dateBoxInstance.focus(); // focus method
+    setTimeout(function () {
+        datetimeBoxInstance.close(); // close method
+        dateBoxInstance.blur(); // bulr method
+
+        dateBoxInstance.beginUpdate(); // beginUpdate method
+        dateBoxInstance.option("value", new Date()); // options method to set value
+        dateBoxInstance.option("disabled", false);
+        dateBoxInstance.endUpdate(); // endUpdate method
+    }, 1500)
+
+    const clearButton = dateBoxInstance.getButton("clear");
+    console.log(clearButton);
+
+    dateBoxInstance.on("valueChanged", function (e) {
+        console.log("New value:", e.value);
+    });
+
+    // Subscribe to multiple events
+    dateBoxInstance.on({
+        opened: function () { console.log("DateBox opened") },
+        closed: function () { console.log("DateBox closed") }
+    });
+
+    //dateBoxInstance.off("opened");
+
+    //datetimeBoxInstance.on("", function (e) {
+    //        console.log(`${e.event.key} Enter key pressed`);
+    //});
+
+    //datetimeBoxInstance.onkeydown((e) => {
+    //    console.log(`${e.event.key} Enter key pressed`);
+    //});
+
+    // dateBoxInstance.dispose();
+    // $("#dateBox").remove();
+});
